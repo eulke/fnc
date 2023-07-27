bbc104a8038814a6f820389f175108f0a17dac48
Esteban Ulke <esteban.ulke@mercadolibre.com>
2023-07-27 09:26:05 -0300

    feat(vcs): add pull functionality to vcs

--- src/main.rs
+++ src/main.rs
@@ -33,6 +33,54 @@ fn handle_new<T: VCSOperations>(
         _ => panic!("Invalid name. Only 'release' and 'hotfix' are allowed."),
     };
 
+    match vcs.validate_status() {
+        Ok(true) => (),
+        Ok(false) => {
+            println!("There are uncommitted changes. Process finished");
+            process::exit(1);
+        }
+        Err(_) => {
+            println!("Error validating status. Process finished");
+            process::exit(1);
+        }
+    }
+
+    match name.as_str() {
+        "release" => {
+            let default_branch = match vcs.get_default_branch() {
+                Ok(branch) => branch,
+                Err(_) => {
+                    println!("Error getting default branch. Process finished");
+                    process::exit(1);
+                }
+            };
+
+            vcs.checkout_branch(&default_branch).unwrap_or_else(|_| {
+                println!("Cannot checkout to the default branch.");
+                process::exit(1);
+            });
+
+            vcs.pull().unwrap_or_else(|_| {
+                println!("Error pulling from remote. Process finished");
+                process::exit(1);
+            });
+        },
+        "hotfix" => {
+            vcs.checkout_branch("master").unwrap_or_else(|_| {
+                vcs.checkout_branch("main").unwrap_or_else(|_| {
+                    println!("Cannot checkout to the master or main branch.");
+                    process::exit(1);
+                });
+            });
+
+            vcs.pull().unwrap_or_else(|_| {
+                println!("Error pulling from remote. Process finished");
+                process::exit(1);
+            });
+        },
+        _ => panic!("Invalid name. Only 'release' and 'hotfix' are allowed."),
+    }
+
     vcs.create_branch(&branch_name).unwrap_or_else(|_| {
         println!("Error crating the branch. Process finished");
         process::exit(1);
@@ -65,7 +113,8 @@ fn handle_new<T: VCSOperations>(
 
 fn main() {
     let cli = Cli::parse();
-    let vcs = git::Adapter;
+
+    let vcs = git::Adapter::new();
 
     let language = match Language::detect() {
         Some(lang) => lang,
--- src/ports.rs
+++ src/ports.rs
@@ -6,9 +6,13 @@ pub struct AuthorInfo {
 }
 
 pub trait VCSOperations {
+    fn new() -> Self;
     fn create_branch(&self, branch_name: &str) -> Result<(), Box<dyn Error>>;
     fn checkout_branch(&self, branch_name: &str) -> Result<(), Box<dyn Error>>;
     fn read_config(&self) -> Result<AuthorInfo, Box<dyn Error>>;
+    fn validate_status(&self) -> Result<bool, Box<dyn Error>>;
+    fn get_default_branch(&self) -> Result<String, Box<dyn Error>>;
+    fn pull(&self) -> Result<(), Box<dyn Error>>;
 }
 
 pub trait PackageOperations {
